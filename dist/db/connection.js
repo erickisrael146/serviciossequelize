f22ff849716380c37a67517fb317c84895bc293b
erick <erizo2002@hotmail.com>
2023-05-30 16:04:00 -0500

    areg

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+const mysql2_1 = __importDefault(require("mysql2"));
 const sequelize = new sequelize_1.Sequelize('freedb_servicios', 'freedb_erick', '8g8bB4C2!nw@kGW', {
     host: 'sql.freedb.tech',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2_1.default,
 });
 exports.default = sequelize;
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,11 @@
 import {Sequelize} from "sequelize";
+import mysql2 from 'mysql2';
 
 const sequelize = new Sequelize('freedb_servicios','freedb_erick','8g8bB4C2!nw@kGW',
     {
         host: 'sql.freedb.tech',
-        dialect: 'mysql'
+        dialect: 'mysql',
+        dialectModule: mysql2,
     });
 
 export default sequelize
