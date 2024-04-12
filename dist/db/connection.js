fdc64afab559b03c6a64d940b1324d271d7a600c
erickisrael146 <82780985+erickisrael146@users.noreply.github.com>
2024-04-11 19:09:58 -0600

    Update connection.js3

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const mysql2_1 = __importDefault(require("mysql2"));
-const sequelize = new sequelize_1.Sequelize('freedb_servicios', 'freedb_erick', '8g8bB4C2!nw@kGW', {
+const sequelize = new sequelize_1.Sequelize('freedb_servicios', 'freedb_erick', 'hARcY&2M&KU93y2', {
     host: 'sql.freedb.tech',
     dialect: 'mysql',
     dialectModule: mysql2_1.default,
