1fc6821483b131234dfa4ff9d8cba23af8dc2c91
erick <erizo2002@hotmail.com>
2025-01-19 21:19:48 -0600

    cambiodebase

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const mysql2_1 = __importDefault(require("mysql2"));
-const sequelize = new sequelize_1.Sequelize('freedb_servicios', 'freedb_erick', '8g8bB4C2!nw@kGW', {
+const sequelize = new sequelize_1.Sequelize('freedb_servicioss', 'freedb_erick', 'tHu?Y6p#9%cMr#2', {
     host: 'sql.freedb.tech',
     dialect: 'mysql',
     dialectModule: mysql2_1.default,
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 import mysql2 from "mysql2";
 
-const sequelize = new Sequelize('freedb_servicios','freedb_erick','8g8bB4C2!nw@kGW',
+const sequelize = new Sequelize('freedb_servicioss','freedb_erick','tHu?Y6p#9%cMr#2',
     {
         host: 'sql.freedb.tech',
         dialect: 'mysql',
